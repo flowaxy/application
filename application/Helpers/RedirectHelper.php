7027691c6be0c90c6ce28c9700792f3f44d0ffca
ua.lifesheets <97382411+lifesheets@users.noreply.github.com>
2025-04-29 15:34:12 +0300

    Redirects the user back to the referring page and optionally sets a flash message.

--- application/Helpers/RedirectHelper.php
+++ application/Helpers/RedirectHelper.php
@@ -30,4 +30,17 @@ class RedirectHelper
         exit;
     }
 
+    /**
+     * Redirects the user back to the referring page and optionally sets a flash message.
+     * Falls back to '/' if no referrer is available.
+     *
+     * @param string $message Optional message to be stored in session as flash data.
+     * @param string $type The flash message type (e.g., 'error', 'success').
+     * @return void
+     */
+    public static function redirectBack(string $message = '', string $type = 'error'): void
+    {
+        $referer = $_SERVER['HTTP_REFERER'] ?? '/';
+        self::redirect($referer, $message, $type);
+    }
 }
