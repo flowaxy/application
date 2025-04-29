ff3c7b89e5f8d0890d4fe51720717eb479453597
ua.lifesheets <97382411+lifesheets@users.noreply.github.com>
2025-04-29 15:33:53 +0300

    Create RedirectHelper.php

---
+++ application/Helpers/RedirectHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Application\Helpers;
+
+use Application\Core\Session;
+
+/**
+ * Class RedirectHelper
+ *
+ * Provides methods for HTTP redirection with optional flash messaging.
+ *
+ * @package App\Helpers
+ */
+class RedirectHelper
+{
+}
