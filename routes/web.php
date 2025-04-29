3109c6894488642f30545b31d8d54ca33159e7e7
ua.lifesheets <97382411+iteffa-works@users.noreply.github.com>
2025-04-29 17:27:11 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,8 @@
 declare(strict_types=1);
 
 use Application\Controllers\SiteController;
-use Application\Core\App;
-use Application\Routing\Router;
 
-/**
- * Define web routes for the application.
- *
- * Here we bind the routes to their corresponding controllers and actions.
- */
-
-// Retrieve the router instance from the application container
-$router = App::get(Router::class);
-
-// Define the homepage route
-$router->get('/', [SiteController::class, 'index']);
+return [
+    // Homepage
+    '/' => [SiteController::class, 'index'],
+];
