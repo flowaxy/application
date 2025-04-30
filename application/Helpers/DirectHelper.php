a1b511fd9926b95ce8b8ee394cf96454e25ad6fe
ua.lifesheets <97382411+iteffa-works@users.noreply.github.com>
2025-04-30 22:05:11 +0300

    Checks for the existence of a directory at a relative path from the root.

--- application/Helpers/DirectHelper.php
+++ application/Helpers/DirectHelper.php
@@ -15,4 +15,15 @@ class DirectHelper
         return is_file(base_path($path));
     }
 
+    /**
+     * Перевіряє існування директорії за відносним шляхом від кореня.
+     *
+     * @param string $path
+     * @return bool
+     */
+    public static function e_dir(string $path): bool
+    {
+        return is_dir(base_path($path));
+    }
+
 }
