cab8f22d5edc495f543ee4bf7a35ef878e846795
ua.lifesheets <97382411+iteffa-works@users.noreply.github.com>
2025-05-01 02:32:30 +0300

    Preloaders (loading animation)

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -39,3 +39,39 @@
 var body = document.body;
 var $windowWidth = $(window).width();
 
+/**
+ * 1. Прелоадери (анімація завантаження)
+ */
+
+// Додаємо клас "loaded" до тіла документа після повного завантаження сторінки
+window.addEventListener("load", function () {
+    document.body.classList.add("loaded");
+});
+
+// Отримуємо тип прелоадера з атрибута data-preloader
+var preloaderType = body.getAttribute("data-preloader");
+
+// Додаємо прелоадер типу 1, якщо вибрано варіант "1"
+if (preloaderType === "1") {
+    var preloader1 = document.createElement("div");
+    preloader1.className = "preloader preloader-1";
+    preloader1.innerHTML = "<div><svg class='loader-circular' viewBox='25 25 50 50'><circle class='loader-path' cx='50' cy='50' r='20'></svg></div>";
+    body.appendChild(preloader1);
+}
+
+// Додаємо прелоадер типу 2, якщо вибрано варіант "2"
+else if (preloaderType === "2") {
+    var preloader2 = document.createElement("div");
+    preloader2.className = "preloader preloader-2";
+    preloader2.innerHTML = "<div><span></span></div>";
+    body.appendChild(preloader2);
+}
+
+// Додаємо прелоадер типу 3, якщо вибрано варіант "3"
+else if (preloaderType === "3") {
+    var preloader3 = document.createElement("div");
+    preloader3.className = "preloader preloader-3";
+    preloader3.innerHTML = "<div><span></span></div>";
+    body.appendChild(preloader3);
+}
+
